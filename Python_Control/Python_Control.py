d6b0752df5395dc3609cb99938a8ed565033a5bb
Lars-eh <lars.he.ehrlich@gmail.com>
2017-05-17 12:09:19 +0800

    WIP

--- Python_Control/Python_Control.py
+++ Python_Control/Python_Control.py
@@ -41,25 +41,6 @@ serialport = input("Enter Xbee Serialport: ")
 print("Establishing connection to: %s" % serialport)
 ser = serial.Serial(serialport, 9600, timeout=1)
 
-def getcom():
-    for port in ports:
-        print(port)
-    print("Available Ports")
-
-def move(angle):
-    ser.write(angle)
-
-def forward():
-    while True:
-        out = 0
-        pygame.event.pump()
-        out = j.get_axis(1)
-        print('GetAxisY')
-        out = (out * 90.0) + 90.0
-        out = int(out)
-        print(out)
-        move(out)
-        sleep(0.3)
 
 
 
