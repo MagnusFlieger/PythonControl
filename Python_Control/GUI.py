3dfcbc1a8e3deaca35ab38cf06c25464206f0237
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-08-18 17:57:30 +0800

    Add yello color

--- Python_Control/GUI.py
+++ Python_Control/GUI.py
@@ -1,11 +1,12 @@
 import pygame
 
 # Define some colors
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
-BLUE =  (  0,   0, 255)
-GREEN = (  0, 255,   0)
-RED =   (255,   0,   0)
+BLACK   =   (   0,   0,   0)
+WHITE   =   ( 255, 255, 255)
+BLUE    =   (   0,   0, 255)
+GREEN   =   (   0, 255,   0)
+RED     =   ( 255,   0,   0)
+YELLOW  =   ( 255, 255,   0)
 
 # Progress bar constants
 PROGRESSBAR_WIDTH = 300
