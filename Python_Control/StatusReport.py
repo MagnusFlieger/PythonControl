6d133e5a7619f9187ee8d60be177f9f291e32648
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-09-07 08:24:46 +0800

    WIP

--- Python_Control/StatusReport.py
+++ Python_Control/StatusReport.py
@@ -1,31 +1,84 @@
 """
 This module provides the class StatusReport
 """
+
+import Comm
+
 class StatusReport:
     """
     Stores all necessary information regarding the status of the MagnusFlieger
     """
-    def __init__(self, everythingOK=True, motorOK=True, servosOK=True, arduinoOK=True):
-        self.everythingOK = everythingOK
-        self.motorOK = motorOK
-        self.servosOK = servosOK
-        self.arduinoOK = arduinoOK
+    def __init__(self, everything_ok=True, motor_ok=True, servos_ok=True, arduino_ok=True,
+                 battery_ok=True, sensors_ok=True, flight_rec_ok=True):
+        if everything_ok:
+            self.everything_ok = True
+            self.motor_ok = True
+            self.servos_ok = True
+            self.arduino_ok = True
+            self.battery_ok = True
+            self.sensors_ok = True
+            self.flight_rec_ok = True
+        else:
+            self.everything_ok = False
+            self.motor_ok = motor_ok
+            self.servos_ok = servos_ok
+            self.arduino_ok = arduino_ok
+            self.battery_ok = battery_ok
+            self.sensors_ok = sensors_ok
+            self.flight_rec_ok = flight_rec_ok
 
     def to_detailed_string(self):
         """
         Returns a detailed message about the status
         """
-        return ""
+        message = ""
+
+        if self.everything_ok:
+            message += "Everything is ok. \n"
+        message += str(self.motor_ok)
+        message += str(self.servos_ok)
+        message += str(self.arduino_ok)
+
+        return message
 
     def to_short_string(self):
         """
         Returns a short message
         """
-        return ""
+        if self.everything_ok:
+            return "Everything is ok."
+        return "Errors found"
+
+    def parse_char(self, char):
+        """
+        Will change a status of this status report depending
+        on the character
+        """
+        if char == Comm.R_STATUS_OK:
+            self.everything_ok = True
 
     @staticmethod
     def empty_statusreport():
         """
         Return an empty status report
         """
-        return StatusReport(True, True, True, True)
+        return StatusReport(True)
+
+    @staticmethod
+    def from_status_chars(chars):
+        """
+        Return a new status report instance based on the infos given
+        by the status chars
+        """
+        for char in chars:
+            if char == Comm.R_STATUS_OK:
+                return StatusReport(True)
+
+    @staticmethod
+    def is_status_char(char):
+        """
+        Returns wheter the given char is a valid status char
+        """
+        if char in Comm.R_STATUSES:
+            return True
+        return False
